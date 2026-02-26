1c969ff766822846533110cbafd8b9da8c51bc82
pablosantamaria26 <santamariapablodaniel@gmail.com>
2026-02-26 16:48:27 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mercadolimpio-v3';
+const CACHE_NAME = 'mercadolimpio-v4';
 const urlsToCache = [
   './',
   './index.html',
@@ -20,4 +20,5 @@ self.addEventListener('fetch', event => {
   );
 
 });
+
 
